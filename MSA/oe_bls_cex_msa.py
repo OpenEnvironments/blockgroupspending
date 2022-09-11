f4b51b06bb81ecb635a69686a8b3091d55c25da2
Michael Bryan <41010416+michaelbbryan@users.noreply.github.com>
2022-09-11 14:53:17 -0400

    Update oe_bls_cex_msa.py

--- MSA/oe_bls_cex_msa.py
+++ MSA/oe_bls_cex_msa.py
@@ -41,13 +41,56 @@ def oe_bls_cex_msa_download(years, regions, msadir, cexurl):
             wget.download(cexurl+'tables/geographic/mean/'+
                           'cu-msa-'+region+'-2-year-average-'+year+'.xlsx',
                       out=msadir,
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# oe_bls_cex_pumd
+## Overview
+---
+This process
+- downloads the MSA summary spreadsheets for a set of years
+- opens and combines them into pandas dataframe then
+- maps the rows to CEX variables
+
+The MSA level data are not geographically inclusive of the US population,
+and the BLS has only selected the larges 2 dozen MSAs,
+so this data may be only useful as providing a control to the model results.
+"""
+
+import pandas as pd
+import wget
+import zipfile
+import os
+from functools import reduce
+import warnings
+warnings.simplefilter("ignore")
+
+
+def oe_bls_cex_msa_download(years, regions, msadir, cexurl):
+    
+    # Avoid conflicts with any previously downloaded
+        
+    try:
+        os.mkdir(msadir)
+    except OSError as err:
+        print()
+        print("Destination directory '"+msadir+"already exists.")
+        print("Please remove it or select a new destination directory.")
+        print()
+        raise err
+
+    for year in years:
+        for region in regions:
+            wget.download(cexurl+'tables/geographic/mean/'+
+                          'cu-msa-'+region+'-2-year-average-'+year+'.xlsx',
+                      out=msadir,
                       bar=None)
     #For example:
     #    wget https://www.bls.gov/cex/tables/geographic/mean/cu-msa-midwest-2-year-average-2020.xlsx
     
     return None
     
-def oe_bls_cex_msa_open(year, regions, msadir=MSADIR):
+def oe_bls_cex_msa_open(year, regions, msadir):
     """
     For a given year and set of Census regions, this function
         opens the MSA summary spreadsheets
@@ -63,6 +106,18 @@ def oe_bls_cex_msa_open(year, regions, msadir=MSADIR):
     
     :return msa:      a dataframe with Items ("Income",Food","Shelter", eg) as rows
                         and MSAs as columns ("Detroit","Chicago","Philadelphia") eg
+
+    Notes
+    -----
+    The MSA summary reports have 45 categories of spending (some Title rows, one calculation)
+    and 27 columns - 4 regional totals with 23 largest MSAs. 
+
+    We need to add the CEX Variable name associated with each report row.
+    and replace the reports MSA column names with related CPI codes.
+        
+    * The CEX uses the MSA as its Primary Sampling Unit (PSU) so the CEX's PSU column has MSA
+    codes in it. Unfortunately the BLS maintains its own version of MSA codes called CPI
+    Codes.
 
     The two reference sheets are manually curated from:
     
@@ -94,11 +149,24 @@ def oe_bls_cex_msa_open(year, regions, msadir=MSADIR):
                         pd.merge(left,right,on=['Item'], how='outer'), 
                         filelist)
 
+    # Replace the city names with CPI codes
     msa.drop([c for c in msa.columns if "All consumer units in" in c],
              axis=1,
              inplace=True)
     
-    return msa
+    msadict = {}
+    for i,m in msacodes.iterrows():
+        msadict[m.Short] = m.Area
+    for i in range(msa.shape[1]):
+        if i == 0:
+            colcodes = ['Item']
+        else:
+            colcodes.append(msadict[msa.columns[i]])
+    msa.columns = colcodes
+    
+    msacoded = pd.merge(msa,cexvariables,left_on="Item",right_on="ReportTitle",how="inner")
+
+    return msacoded
 
 if __name__ == "__main__":
     
@@ -116,8 +184,3 @@ if __name__ == "__main__":
     
     print("Done")
 
-# 45 categories of spending (some subtotals)
-# 27 columns - 4 regional totals with 3 largest MSAs
-
-# BLS uses the MSA as its Primary Sampling Unit (PSU)
-# So the PSU column has MSA codes in it
